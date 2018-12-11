f78601fd6a1566ef99e2d8606c242e2d2ce37ddc
MayBMore <43025864+MayBMore@users.noreply.github.com>
2018-12-11 15:21:15 +0900

    21

--- movie.c
+++ movie.c
@@ -13,12 +13,8 @@ typedef struct movInfo{
 
 void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 {
-	FILE *fp;
 	movInfo_t* mvPtr; //struct mvPtr
 	
-	while (fp != NULL) {
-		fscanf(fp, "%s %6f %i %s", mvPtr->name, mvPtr->score, mvPtr->runTime, mvPtr->madeIn); 
-	}
 	
 	return (void*)mvPtr;
 }
